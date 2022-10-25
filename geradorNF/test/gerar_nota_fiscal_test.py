a16269a1f37bba750cd00190bca4921bcdf58acd
luanabm <38760797+luanabm@users.noreply.github.com>
2022-10-24 23:41:17 -0300

    Update gerar_nota_fiscal_test.py

--- geradorNF/test/gerar_nota_fiscal_test.py
+++ geradorNF/test/gerar_nota_fiscal_test.py
@@ -3,8 +3,8 @@ from src.TipoFatura import TipoFatura
 from src.GeradorNF import GeradorNF
 
 def test_gera_nota_fiscal():
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.CONSULTORIA
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
@@ -15,8 +15,8 @@ def test_gera_nota_fiscal():
   assert nf.cliente == nome_cliente
 
 def test_gera_nota_fiscal_25_imposto():
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.CONSULTORIA
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
@@ -28,8 +28,8 @@ def test_gera_nota_fiscal_25_imposto():
   assert nf.imposto == valor_fatura * 0.25
 
 def test_gera_nota_fiscal_15_imposto():
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.TREINAMENTO
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
@@ -41,8 +41,8 @@ def test_gera_nota_fiscal_15_imposto():
   assert nf.imposto == valor_fatura * 0.15
 
 def test_gera_nota_fiscal_6_imposto():
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.AVULSA
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
