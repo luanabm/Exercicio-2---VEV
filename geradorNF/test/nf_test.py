229a28cdaa367590978a2f1a603a777c4d9224e7
luanabm <38760797+luanabm@users.noreply.github.com>
2022-10-24 23:41:51 -0300

    Update nf_test.py

--- geradorNF/test/nf_test.py
+++ geradorNF/test/nf_test.py
@@ -7,8 +7,8 @@ from src.GeradorNF import GeradorNF
 def deve_salvar_nf_no_banco():
   repository = NFRepository()
   
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.CONSULTORIA
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
