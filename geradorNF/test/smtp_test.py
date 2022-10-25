e0f32307b0db3bc6316d7581bacf5d39d1d0b9fd
luanabm <38760797+luanabm@users.noreply.github.com>
2022-10-24 23:42:31 -0300

    Update smtp_test.py

--- geradorNF/test/smtp_test.py
+++ geradorNF/test/smtp_test.py
@@ -1,12 +1,12 @@
-from src.services.SMTPService import SMTP
-from src.enums.TipoFatura import TipoFatura
-from src.entities.Fatura import Fatura
-from src.usecases.GeradorNF import GeradorNF
+from src.SMTPService import SMTP
+from src.TipoFatura import TipoFatura
+from src.Fatura import Fatura
+from src.GeradorNF import GeradorNF
 
 def test_deve_enviar_smtp():
   smtp = SMTP()
-  nome_cliente = "Carlos"
-  endereco_cliente = "Rua A"
+  nome_cliente = "Amanda"
+  endereco_cliente = "Rua Arnald"
   tipo_servico = TipoFatura.CONSULTORIA
   valor_fatura = 1000
   fatura = Fatura(nome_cliente, endereco_cliente, tipo_servico, valor_fatura)
